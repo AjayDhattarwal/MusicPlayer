4cd64930ad5807c6bc23f1b738ac76f99c427bb5
kalios <trojanstar5776@gmail.com>
2024-06-11 11:19:24 +0530

    Info Screen

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,11 @@
 plugins {
-    id("com.android.application")
-    id("org.jetbrains.kotlin.android")
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.jetbrains.kotlin.android)
+    id("com.google.devtools.ksp")
+    id ("com.google.dagger.hilt.android")
+    alias(libs.plugins.google.gms.google.services)
+    alias(libs.plugins.kotlin.serialization)
+    id("kotlin-parcelize")
 }
 
 android {
@@ -9,7 +14,7 @@ android {
 
     defaultConfig {
         applicationId = "com.ar.musicplayer"
-        minSdk = 24
+        minSdk = 29
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
@@ -23,10 +28,7 @@ android {
     buildTypes {
         release {
             isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
         }
     }
     compileOptions {
@@ -51,19 +53,66 @@ android {
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.12.0")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
-    implementation("androidx.activity:activity-compose:1.8.2")
-    implementation(platform("androidx.compose:compose-bom:2023.08.00"))
-    implementation("androidx.compose.ui:ui")
-    implementation("androidx.compose.ui:ui-graphics")
-    implementation("androidx.compose.ui:ui-tooling-preview")
-    implementation("androidx.compose.material3:material3")
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    androidTestImplementation(platform("androidx.compose:compose-bom:2023.08.00"))
-    androidTestImplementation("androidx.compose.ui:ui-test-junit4")
-    debugImplementation("androidx.compose.ui:ui-tooling")
-    debugImplementation("androidx.compose.ui:ui-test-manifest")
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(libs.androidx.activity.compose)
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.ui)
+    implementation(libs.androidx.ui.graphics)
+    implementation(libs.androidx.ui.tooling.preview)
+    implementation(libs.androidx.material3)
+    implementation(libs.androidx.compose.material)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+    androidTestImplementation(platform(libs.androidx.compose.bom))
+    androidTestImplementation(libs.androidx.ui.test.junit4)
+    debugImplementation(libs.androidx.ui.tooling)
+    debugImplementation(libs.androidx.ui.test.manifest)
+
+    implementation(libs.navigation.compose)
+
+    implementation ("androidx.compose.runtime:runtime-livedata:1.6.7")
+    implementation ("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.1")
+
+
+    implementation(platform("io.coil-kt:coil-bom:2.4.0"))
+    implementation("io.coil-kt:coil-compose")
+    implementation("io.coil-kt:coil-svg")
+    implementation("io.coil-kt:coil-gif")
+
+    implementation ("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation ("com.google.code.gson:gson:2.10.1")
+
+    implementation ("com.squareup.okhttp3:logging-interceptor:4.10.0")
+    implementation ("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation ("com.squareup.okhttp3:okhttp:4.11.0")
+
+
+    // Dagger hilt
+    implementation (libs.hilt.android)
+    ksp (libs.hilt.compiler)
+
+    // For instrumentation tests
+    androidTestImplementation  (libs.hilt.android.testing)
+    kspAndroidTest (libs.hilt.compiler)
+
+    // For local unit tests
+    testImplementation (libs.hilt.android.testing)
+    kspTest (libs.hilt.compiler)
+
+    // Extended Icons
+    implementation ("androidx.compose.material:material-icons-extended: 1.5.1")
+
+    // Navigation
+    implementation(libs.kotlinx.serialization.json)
+    implementation("androidx.hilt:hilt-navigation-compose:1.0.0")
+
+    //image color
+    implementation ("androidx.palette:palette-ktx:1.0.0")
+
+
+
 }
--- app/src/main/java/com/ar/musicplayer/MainActivity.kt
+++ app/src/main/java/com/ar/musicplayer/MainActivity.kt
@@ -3,44 +3,78 @@ package com.ar.musicplayer
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.ar.musicplayer.ui.theme.MusicPlayerTheme
+import androidx.activity.viewModels
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.navigation.compose.rememberNavController
+import com.ar.musicplayer.di.NetworkModule_ProvideApiServiceFactory.provideApiService
+import com.ar.musicplayer.models.HomeData
+import com.ar.musicplayer.navigation.App
+import com.ar.musicplayer.viewmodel.HomeViewModel
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.serialization.Serializable
 
+
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+
+    private val viewModel: HomeViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContent {
-            MusicPlayerTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
-            }
+            val navController = rememberNavController()
+            App(navController = navController,homeViewModel = viewModel)
+
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MusicPlayerTheme {
-        Greeting("Android")
-    }
-}
+
+
+
+
+
+
+
+
+
+
+
+
+//composable<ScreenC>(typeMap = mapOf(typeOf<SongItem>() to SongType )) {
+//    val args = it.toRoute<ScreenC>()
+//    val songItem = args.songItem
+//    Column(modifier = Modifier.fillMaxSize(),
+//        verticalArrangement = Arrangement.Center,
+//        horizontalAlignment = Alignment.CenterHorizontally
+//    ) {
+//        songItem.title?.let { it1 -> Text(text = it1) }
+//    }
+//}
+
+//@Serializable
+//data class ScreenC(
+//    val songItem: SongItem
+//)
+
+//val SongType = object : NavType<SongItem>(
+//    isNullableAllowed = false
+//) {
+//    override fun get(bundle: Bundle, key: String): SongItem? =
+//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+//            bundle.getParcelable(key, SongItem::class.java)
+//        } else {
+//            @Suppress("DEPRECATION") // for backwards compatibility
+//            bundle.getParcelable(key)
+//        }
+//
+//    override fun put(bundle: Bundle, key: String, value: SongItem) =
+//        bundle.putParcelable(key, value)
+//
+//    override fun parseValue(value: String): SongItem = Json.decodeFromString(value)
+//
+//    override fun serializeAsValue(value: SongItem): String = Json.encodeToString(value)
+//    override val name: String = "SongItem"
+//}
